86205cd7fe6c4d1e07741eee317506373f9496a1
Akeem <akeem.mighty@gmail.com>
2022-04-04 12:12:18 -0400

    Changed Database Settings + Created super_types App

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3pgrdkh$#@j-ley@k0$v@!#o+*vucexn@1^=vz^mf@r=#eu7*o'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -69,16 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
