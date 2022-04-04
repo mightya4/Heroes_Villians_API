73d8f0df49cddba513eaffc7a423cad8e34d3d72
Akeem <akeem.mighty@gmail.com>
2022-04-04 16:28:15 -0400

    Created supers App Serializer File and Class+ Created super_types App Serializer File and Class

---
+++ supers/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Supers
+
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supers
+        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type_id']
+        
